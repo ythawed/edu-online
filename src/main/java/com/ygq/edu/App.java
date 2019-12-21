eaad4fa7bcc244bf6753e959b706b08c5076126d
ythawed <1375476814@qq.com>
2019-12-21 14:49:50 +0800

    第一次提交：项目正常运行，跑通APP

---
+++ src/main/java/com/ygq/edu/App.java
@@ -0,0 +1,16 @@
+package com.ygq.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ythawed
+ * @date 2019/12/21 0021
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/ygq/edu/controller/Test.java
@@ -0,0 +1,19 @@
+package com.ygq.edu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 测试启动是否正常
+ * @author ythawed
+ * @date 2019/12/21 0021
+ */
+@Controller
+public class Test {
+
+    @RequestMapping(value = "/test/{name}",method = RequestMethod.GET)
+    @ResponseBody
+    public String test(@PathVariable(value = "name") String name) {
+        return "Hello:" + name;
+    }
+}
