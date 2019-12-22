86cc1f70bbad05c926666d46b5f5950c4cf3e5ae
ythawed <1375476814@qq.com>
2019-12-22 13:50:03 +0800

    第3次提交，mybatis逆向工程生成数据表实体类。拿Chapter类做测试

---
+++ src/main/java/com/ygq/edu/controller/VideoController.java
@@ -0,0 +1,11 @@
+package com.ygq.edu.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ythawed
+ * @date 2019/12/21 0021
+ */
+@RestController
+public class VideoController {
+}
